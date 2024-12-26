827154a4000658f8c2d18e5ed16bed885edbf808
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-12-26 19:17:03 +0600

    Add view to show all comments

--- my_first_app/views.py
+++ my_first_app/views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, CommentSerializer
+from .other_classes import Comment
 
 # Create your views here.
 @api_view(['GET', 'POST'])
@@ -15,3 +16,9 @@ def product_list_view(request):
         'products': serialized_products.data
     }
     return Response(context)
+
+
+def comment_list_view(request):
+    comment = Comment('arefin@gmail.com', 'This is very good product')
+    serialized_comment = CommentSerializer(comment, many=True)
+    return Response(serialized_comment.data)
