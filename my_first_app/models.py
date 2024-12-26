d66f3a79ccd51ee90bfd1cd8e76a38b943d590bd
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-12-26 16:06:02 +0600

    Create Product model

--- my_first_app/models.py
+++ my_first_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_code = models.BigIntegerField()
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(max_length=200)
+    exp_date = models.DateField()
+
+    def __str__(self):
+        return self.name
