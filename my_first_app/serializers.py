00a748ab626b2f18a28174043a3cda26f51f4553
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-12-26 19:07:29 +0600

    Add simple serializer

--- my_first_app/serializers.py
+++ my_first_app/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Product
 
+# model serializer
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+# simple serializer
+class CommentSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    content = serializers.CharField(max_length=200)
+    created = serializers.DateTimeField()
--- my_first_app/views.py
+++ my_first_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from .models import Product
 from .serializers import ProductSerializer
 
 # Create your views here.
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def product_list_view(request):
     products = Product.objects.all()
     serialized_products = ProductSerializer(products, many=True)
